fbe46830aa1aa1444a4831ce0960e924a5158351
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2024-01-19 00:47:58 +0530

    Update Insertion Sort.py

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -21,7 +21,10 @@ Output: [1 2 3 4 6 13 28]
 Explanation: After applying insertion sort on the input array, the output is [1 2 3 4 6 13 28].
 '''
 
-# Solution 1: 
+# Solution 1: Insertion Sort
+# Time Complexity - O(N^2), where ‘N’ is the number of length of array.
+# Space Complexity - O(1)
+
 def insertionSort(arr):
     n = len(arr)
 
@@ -32,5 +35,30 @@ def insertionSort(arr):
             j = j - 1
     return arr
 
-# Solution 2:
+# Solution 2: Recusrion Insertion Sort
 
+'''
+# Time Complexity -  O( N ^ 2 ), where 'N' is the array ‘arr’ size.
+
+In the worst case, we have to do n^2 recursive call on the array ‘arr’,
+
+Hence the time complexity is O( N ^ 2 ).      
+
+# Space Complexity - O( N ), where 'N' is the array ‘arr’ size.
+ 
+We are using O(N) auxiliary stack space,
+
+Hence the space complexity is O( N ).
+'''
+
+def insertionSortRecursion(arr, i, n):
+    if i == n:
+        return
+    j = i
+    while j > 0 and arr[j-1] > arr[j]:
+        arr[j-1], arr[j] = arr[j], arr[j-1]
+        j = j - 1
+    return insertionSortRecursion(arr, i+1,n)
+
+def insertionSort(arr):
+    return insertionSortRecursion(arr, 0, len(arr))
