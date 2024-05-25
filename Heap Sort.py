58d3f453a9db458c11e8f92d5d367ddef0da3cd6
Kannan Paulvannan <41234212+KannanPaul@users.noreply.github.com>
2024-05-25 23:14:52 +0530

    Update Heap Sort.py

--- Heap Sort.py
+++ Heap Sort.py
@@ -20,3 +20,47 @@ Reference
 - https://www.programiz.com/dsa/heap-sort
 - https://www.geeksforgeeks.org/heap-sort/?ref=shm
 ```
+
+Time Complexity: O(N log N)
+Auxiliary Space: O(log n), due to the recursive call stack. However, auxiliary space can be O(1) for iterative implementation.
+
+def heapify(arr, n, i):
+      # Find largest among root and children
+      largest = i
+      l = 2 * i + 1
+      r = 2 * i + 2
+  
+      if l < n and arr[i] < arr[l]:
+          largest = l
+  
+      if r < n and arr[largest] < arr[r]:
+          largest = r
+  
+      # If root is not largest, swap with largest and continue heapifying
+      if largest != i:
+          arr[i], arr[largest] = arr[largest], arr[i]
+          heapify(arr, n, largest)
+  
+  Solution 1:
+  
+  def heapSort(arr):
+      n = len(arr)
+  
+      # Build max heap
+      for i in range(n//2, -1, -1):
+          heapify(arr, n, i)
+  
+      for i in range(n-1, 0, -1):
+          # Swap
+          arr[i], arr[0] = arr[0], arr[i]
+  
+          # Heapify root element
+          heapify(arr, i, 0)
+  
+  
+  arr = [1, 12, 9, 5, 6, 10]
+  heapSort(arr)
+  n = len(arr)
+  print("Sorted array is")
+  for i in range(n):
+      print("%d " % arr[i], end='')
